76b2786ae8c7308be77b95f7b84001c7da00df85
Kaizar Hub <kaizarhnd@gmail.com>
2025-04-20 21:35:32 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  const menuLinks = document.querySelector(".menu-links");
+  menuLinks.classList.toggle("show");
+}
+
+
+function toggleTheme() {
+  document.body.classList.toggle("dark")
+}
